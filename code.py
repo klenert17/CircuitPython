cd6dbba5cb57b36afe2f115183b496ae52c52faf
klenert17 <klenert17@charlottesvilleschools.org>
2022-09-12 15:38:16 -0400

    completed neopixle assignment

--- code.py
+++ code.py
@@ -1,5 +1,14 @@
-from time import sleep
+# Kathryn Lenert
+# Hello Circuit Python
+# This code makes the light turn colors
+
+import board
+import neopixel
+
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+dot.brightness = 0.2
+
+print("Make it aqua!")
 
 while True:
-    print("Hello world!")
-    sleep(1)
+    dot.fill((255, 0, 255)) 
