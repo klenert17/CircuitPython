a11d73d1eba0de1d5df06aa58e9ffc444b633320
klenert17 <klenert17@charlottesvilleschools.org>
2022-09-16 15:27:56 -0400

    completed servo assignment

---
+++ myservo.py
@@ -0,0 +1,22 @@
+# Kathryn Lenert
+# Servo Turn Python
+# This code makes a servo spin to the left and reset itself back
+
+
+import time 
+import board 
+import pwmio
+from adafruit_motor import servo
+
+
+pwm = pwmio.PWMOut(board.D3, duty_cycle=2 ** 15, frequency=50) #using the PWMOut function within the pwmio library 
+
+
+my_servo = servo.Servo(pwm)
+
+while True:
+    for angle in range(0, 180, 5): #the servo rotates 180 degrees 
+        my_servo.angle = angle
+        time.sleep(0.05) #pauses for 0.05 seconds
+    for angle in range(180, 0, -5): #the servo rotates 180 degrees ther other way 
+        time.sleep(0.05) #pauses for 0.05 seconds
