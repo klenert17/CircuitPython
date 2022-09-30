f555182f3e7a257dbf5a1b888ea8452a91bc7319
klenert17 <klenert17@charlottesvilleschools.org>
2022-09-30 15:32:43 -0400

    completes 2 button servo code

--- myservo.py
+++ myservo.py
@@ -2,21 +2,39 @@
 # Servo Turn Python
 # This code makes a servo spin to the left and reset itself back
 
-
-import time 
-import board 
+import time
+import board
 import pwmio
 from adafruit_motor import servo
+from digitalio import DigitalInOut, Direction, Pull
 
+btn1 = DigitalInOut(board.D8)
+btn1.direction = Direction.INPUT
+btn1.pull = Pull.DOWN
 
-pwm = pwmio.PWMOut(board.D3, duty_cycle=2 ** 15, frequency=50) #using the PWMOut function within the pwmio library 
+btn2 = DigitalInOut(board.D9)
+btn2.direction = Direction.INPUT
+btn2.pull = Pull.DOWN 
 
+# create a PWMOut object on Pin A2.
+pwm = pwmio.PWMOut(board.D10, duty_cycle=2 ** 15, frequency=50)
+# Create a servo object, my_servo.
 
-my_servo = servo.Servo(pwm)
+my_servo = servo.Servo(pwm, min_pulse=600, max_pulse=2700)
+
+angle = 90 
 
 while True:
-    for angle in range(0, 180, 5): #the servo rotates 180 degrees 
-        my_servo.angle = angle
-        time.sleep(0.05) #pauses for 0.05 seconds
-    for angle in range(180, 0, -5): #the servo rotates 180 degrees ther other way 
-        time.sleep(0.05) #pauses for 0.05 seconds
+   if btn1.value and angle <180:
+      print("btn1 pressed")
+      angle = angle +5 
+      my_servo.angle = angle 
+      time.sleep(0.01)
+   elif btn2.value and angle >0:
+      print("btn2 pressed")
+      angle = angle -5 
+      my_servo.angle = angle 
+      time.sleep(0.01)
+   else: 
+      print("servo off")
+      time.sleep(1)
